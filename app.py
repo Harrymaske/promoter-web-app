1dc8ba517f14b239c5a0056c5850adcc0cdd2f5d
Harrymaske <158072689+Harrymaske@users.noreply.github.com>
2025-07-17 09:59:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,6 @@ def index():
 
     return render_template("index.html", prediction=prediction, confidence=confidence)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+
